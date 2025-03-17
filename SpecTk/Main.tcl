b388ea7c70332bee67253fee7701ef8ca26078d2
Daniel Kaloyanov <kaloyano@steelhead.nscl.msu.edu>
2025-03-17 00:13:52 -0400

    SpecTk 1.7.6

--- SpecTk/Main.tcl
+++ SpecTk/Main.tcl
@@ -44,7 +44,7 @@ source $SpecTkHome/List.tcl
 
 proc SetupSpecTk {} {
 	global spectk
-	set spectk(version) "1.7.5"
+	set spectk(version) "1.7.6"
 	set spectk(configName) unknown.spk
 	set spectk(smartmenu) .
 	set spectk(smartprevious) .
--- SpecTk/Wave2D.tcl
+++ SpecTk/Wave2D.tcl
@@ -342,20 +342,19 @@ itcl::body Wave2D::CalculateROI {roi} {
 	set calc($roi) [format %.7g $sz]
 	set total [lindex $calc(All) 0]
 
-	set xm [expr [blt::vector expr sum(z*(x*$xinc+$xlow))] / $sz] 
-	set ym [expr [blt::vector expr sum(z*(y*$yinc+$ylow))] / $sz]
-
-	set xStd [expr sqrt([blt::vector expr sum(z*((x*$xinc+$xlow)-$xm)^2)] / $sz)]
-	set yStd [expr sqrt([blt::vector expr sum(z*((y*$yinc+$ylow)-$ym)^2)] / $sz)]
-
-	set cov [expr [blt::vector expr {sum(z * ((x*$xinc+$xlow)-$xm) * ((y*$yinc+$ylow)-$ym))}] / $sz]
-
-	set thetaRad [expr {.5 * atan2(2*$cov, $xStd*$xStd - $yStd*$yStd)}]
-	set thetaDeg [expr {$thetaRad * 180.0 / acos(-1)}]
-
 	if {$sz == 0} {
 		lappend calc($roi) 0 0 0 0 0 0 0 0 0
 	} else {
+		set xm [expr [blt::vector expr sum(z*(x*$xinc+$xlow))] / $sz] 
+		set ym [expr [blt::vector expr sum(z*(y*$yinc+$ylow))] / $sz]
+
+		set xStd [expr sqrt([blt::vector expr sum(z*((x*$xinc+$xlow)-$xm)^2)] / double($sz-1))]
+		set yStd [expr sqrt([blt::vector expr sum(z*((y*$yinc+$ylow)-$ym)^2)] / double($sz-1))]
+
+		set cov [expr [blt::vector expr {sum(z * ((x*$xinc+$xlow)-$xm) * ((y*$yinc+$ylow)-$ym))}] / double($sz-1)]
+
+		set thetaRad [expr {.5 * atan2(2*$cov, $xStd*$xStd - $yStd*$yStd)}]
+		set thetaDeg [expr {$thetaRad * 180.0 / acos(-1)}]
 		lappend calc($roi) [format %.5g [expr (100.0*$sz)/$total]]
 		lappend calc($roi) [set xm [format %.5g [expr [blt::vector expr sum(z*(x*$xinc+$xlow))] / $sz]]]
 		lappend calc($roi) [set ym [format %.5g [expr [blt::vector expr sum(z*(y*$yinc+$ylow))] / $sz]]]
